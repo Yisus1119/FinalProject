d79ac9be68f17956bf448821932a0ab05c0ac1dc
Enrique <Enriquemk51@gmail.com>
2021-12-09 12:24:20 -0400

    Conexion a la base de datos

---
+++ ClaseDatos/Conexion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ClaseDatos
+{
+    internal class Conexion
+    {
+        SqlConnection con;
+        public Conexion()
+        {
+            con = new SqlConnection("Server=DESKTOP-2TK8MGE; Database=Mi Primera Vez; Integrated security=true");
+        }
+        public SqlConnection Conectar()
+        {
+            try
+            {
+                con.Open();
+                return con;
+                MessageBox.Show("Conexion exitosa");
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+        public bool Desconectar()
+        {
+            try
+            {
+                con.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ClaseDatos/Metodos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseDatos
+{
+    internal class Metodos
+    {
+        public DataTable Mostrar()
+        {
+
+            comando.Connection = Conexion.Conectar();
+            comando.CommandText = "MostrarProductos";
+            comando.CommandType = CommandType.StoredProcedure;
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            conexion.CerrarConexion();
+            return tabla;
+
+        }
+    }
+}
