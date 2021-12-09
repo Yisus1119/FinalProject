bffcea4d1c5c65107a522032327b1de2f8572d14
Enrique <Enriquemk51@gmail.com>
2021-12-09 13:52:37 -0400

    Medoto de mostrar datos

--- ClaseDatos/Metodos.cs
+++ ClaseDatos/Metodos.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,20 @@ namespace ClaseDatos
 {
     internal class Metodos
     {
+
+        private Conexion conexion = new Conexion();
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
         public DataTable Mostrar()
         {
 
-            comando.Connection = Conexion.Conectar();
+            comando.Connection = conexion.Conectar();
             comando.CommandText = "MostrarProductos";
             comando.CommandType = CommandType.StoredProcedure;
             leer = comando.ExecuteReader();
             tabla.Load(leer);
-            conexion.CerrarConexion();
+            conexion.Desconectar();
             return tabla;
 
         }
